fe9551bc2f7f5a054790b288ba081614b9408be9
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-11-16 12:26:52 -0500

    Set up prettier

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
-import concurrently from "concurrently";
+import concurrently from 'concurrently'
 
-concurrently(
-  [
+concurrently([
     {
         name: 'server',
         command: 'bun run dev',
         cwd: 'packages/server',
-        prefixColor: 'cyan'
+        prefixColor: 'cyan',
     },
     {
         name: 'client',
         command: 'bun run dev',
         cwd: 'packages/client',
-        prefixColor: 'green'
+        prefixColor: 'green',
     },
-  ]
-)
+])
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react"
-import { Button } from "./components/ui/button"
+import { useEffect, useState } from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [message, setMessage] = useState<string>("")
+    const [message, setMessage] = useState<string>('')
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error fetching message:", err))
-  }, [])
+    useEffect(() => {
+        fetch('/api/hello')
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message))
+            .catch((err) => console.error('Error fetching message:', err))
+    }, [])
 
-  return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Click me</Button>
-    </div>
-  )
+    return (
+        <div className="p-4">
+            <p className="font-bold text-3xl">{message}</p>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 
 export default App
